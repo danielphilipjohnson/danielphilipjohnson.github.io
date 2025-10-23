a8838bc7d15f4d58e8527840a8852ab66b14c0ff
Daniel Philip Johnson <daniel-philip-johnson@outlook.com>
2025-10-23 18:06:30 +0100

    improve the action

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,9 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
 
+// https://astro.build/config
 export default defineConfig({
   site: "https://danielphilipjohnson.github.io/",
-  base: '/',
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss({ config: { content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'] } })],
