bcb74f3210794f63540c7ba574373a1864def9d1
Daniel Philip Johnson <daniel-philip-johnson@outlook.com>
2025-10-23 17:52:48 +0100

    add base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://danielphilipjohnson.github.io/",
+  base: '/',
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss({ config: { content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'] } })],
